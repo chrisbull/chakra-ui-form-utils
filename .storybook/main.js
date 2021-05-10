99f3b0a81ee54da4541a1cf3dc5e5ef5a292b7d5
Chris Bull <chris@chrisbull.com>
2021-05-10 00:47:21 -0700

    custom config for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,20 @@
+const path = require('path');
+const toPath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-knobs/register'],
+  addons: ['@storybook/addon-knobs/register', 'storybook-dark-mode'],
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          'emotion-theming': toPath('node_modules/@emotion/react'),
+        },
+      },
+    };
+  },
 };
